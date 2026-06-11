d756c476fa514a40025bf81866a5593e202e61f7
fouadhassan74 <fuad16020@gmail.com>
2026-06-11 12:48:00 +0300

    fix issue 1- remove action of leave from owner 2- fix action leave api

--- src/feature/team/types/team.ts
+++ src/feature/team/types/team.ts
@@ -62,6 +62,7 @@ export type TeamRowKind = 'member' | 'invitation';
 
 export interface TeamRowModel {
   id: string;
+  organizationId?: string;
   kind: TeamRowKind;
   name: string;
   email: string;
